c9356e46f471dde371498f5d37ac8abfc32b96b4
Thedor <kons.gasenzer@gmail.com>
2016-10-16 22:11:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ ini_set('display_errors', 1);
 include "system/resources/funct.php"; //Laden von Funktionen (später besonders wichtig bei User- und Adminbereich)
 include "system/resources/var.php";   //Laden von Variablen (dort sind auch die mit $_GET übergebenen Parameter in einem Array definiert
 include "system/resources/const.php"; //Laden von Konstanten (beispielsweise Fehlerkonstanten)
-//include "system/resources/classes.php"; //kein Inhalt
+//include "system/resources/class.php"; //kein Inhalt
 
 //Einloggen in Datenbank (bei mir jetzt Passwort und Username im Plain-Phpcode der extern sowieso nicht gelesen werden kann)
                       //Host      User        Passwort      Datenbank
