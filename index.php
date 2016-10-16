c067cc68f84eb8bc8a76a04204e49edb5a0e9143
Thedor <kons.gasenzer@gmail.com>
2016-10-16 22:05:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,4 +11,15 @@
 //Php-Errors erzeugen & anzeigen
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+include "system/resources/funct.php"; //Laden von Funktionen (später besonders wichtig bei User- und Adminbereich)
+include "system/resources/var.php";   //Laden von Variablen (dort sind auch die mit $_GET übergebenen Parameter in einem Array definiert
+include "system/resources/const.php"; //Laden von Konstanten (beispielsweise Fehlerkonstanten)
+//include "system/resources/classes.php"; //kein Inhalt
+
+//Einloggen in Datenbank (bei mir jetzt Passwort und Username im Plain-Phpcode der extern sowieso nicht gelesen werden kann)
+                      //Host      User        Passwort      Datenbank
+$mysql = @new MySQLi('localhost','user','FPn5rQm2mAWt6wVc','database'); //$msql wird jetzt immer benutzt um mit Datenbank zu interagieren
+
+
 ?>
